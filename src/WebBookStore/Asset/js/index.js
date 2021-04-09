cd8897832e6ff72e44e2c64e529b4c51d22f0b29
XeusNg <kobikiu810@gmail.com>
2021-04-09 20:38:40 +0700

    Second Commit

---
+++ src/WebBookStore/Asset/js/index.js
@@ -0,0 +1,28 @@
+const switchRegister = document.querySelector('.switch-link.switch-register');
+const switchLogin = document.querySelector('.switch-link.switch-login');
+const formLogin = document.querySelector('.form-login');
+const formRegister = document.querySelector('.form-register');
+
+//------------------------------Product-Item
+
+const bookNames = document.querySelectorAll(".book-name");
+bookNames.forEach((bookName) => {
+    if (bookName.innerHTML.length > 20) {
+        bookName.innerHTML = bookName.innerHTML.substring(0, 35) + "...";
+    }
+});
+
+
+switchRegister.addEventListener('click', (e) => {
+    e.preventDefault();
+    formRegister.style.transform = 'translateX(-400px)';
+    formLogin.style.transform = 'translateX(-400px)';
+})
+
+switchLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    formRegister.style.transform = 'translateX(0)';
+    formLogin.style.transform = 'translateX(0)';
+})
+
+
---
+++ src/WebBookStore/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBookStore.Models;
+
+namespace WebBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        BookStoreEntities db = new BookStoreEntities();
+        public PartialViewResult BookPartial()
+        {
+            var listbook = db.Books.Take(20).ToList();
+            return PartialView(listbook);
+        }
+        public ViewResult DetailBook(string idbook)
+        {
+            Book book = db.Books.SingleOrDefault(n => n.Id_Book == idbook);
+            if (book == null)
+            {
+                //Trả về phương thức lỗi
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(book);
+        }
+    }
+}
